dad52a9850be8ac81f6449fa54ebd2e60cd82afe
Antoine Roy <antoine.roy77@gmail.com>
2024-02-15 12:47:08 +0100

    test

--- leader.tf
+++ leader.tf
@@ -14,6 +14,12 @@ resource "aws_instance" "leader" {
 
   #PUBLISHING SCRIPTS AND DATA
   key_name = aws_key_pair.loadtest.key_name
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "${tls_private_key.loadtest.public_key_openssh}" >> /home/ec2-user/.ssh/authorized_keys
+            EOF
+            
   connection {
     host        = self.private_ip
     type        = "ssh"
